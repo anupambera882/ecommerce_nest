3eedca142c76266c5703a3a71ad2110a5700a953
“Anupam <anupam@pitangent.com>
2023-10-05 15:28:08 +0530

    auth in mysql

---
+++ src/auth/entity/user.entity.ts
@@ -0,0 +1,33 @@
+import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+
+export enum Role {
+  SUPER_ADMIN = 'superAdmin',
+  LICENSE = 'License',
+  USER = 'User',
+  ADMIN = 'Admin',
+}
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  name: string;
+
+  @Column()
+  email: string;
+
+  @Column()
+  phone: string;
+
+  @Column()
+  password: string;
+
+  @Column({
+    type: 'enum',
+    enum: Role,
+    default: Role.USER,
+  })
+  role: Role;
+}
