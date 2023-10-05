3e93ece43f555713351ee4590abd0287c8105089
“Anupam <anupam@pitangent.com>
2023-10-05 18:54:05 +0530

    auth in mysql

--- src/auth/dto/dto/check-email.dto.ts
+++
@@ -1,9 +0,0 @@
-import { IsEmail, IsNotEmpty } from 'class-validator';
-import { ApiProperty } from '@nestjs/swagger';
-
-export class CheckEmailDto {
-  @ApiProperty()
-  @IsNotEmpty()
-  @IsEmail({}, { message: 'Please enter correct email' })
-  readonly email: string;
-}
--- src/auth/dto/dto/forget-password.dto.ts
+++
@@ -1,9 +0,0 @@
-import { IsEmail, IsNotEmpty } from 'class-validator';
-import { ApiProperty } from '@nestjs/swagger';
-
-export class ForgetPasswordDto {
-  @ApiProperty()
-  @IsNotEmpty()
-  @IsEmail({}, { message: 'Please enter correct email' })
-  readonly email: string;
-}
